3f94f76d0095907c97e7b23bac761940266ab49f
Bakr Aldubai <90357810+BakrAldubai@users.noreply.github.com>
2023-06-02 02:31:43 +0300

    Update utils.py

--- pos_compu_sys/utils.py
+++ pos_compu_sys/utils.py
@@ -2,11 +2,11 @@ from erpnext.accounts.utils import get_balance_on
 from frappe.utils import now
 
 def get_balance(doc, method=None):
-    doc.party_balance_before_transiction = get_balance_on(
+    doc.party_balance_before_transaction = get_balance_on(
         account=doc.debit_to,
         date=doc.posting_date,
         party_type="Customer",
         party=doc.customer,
         company=doc.company
     )
-    doc.party_balance_after_transiction = doc.party_balance_before_transiction + doc.outstanding_amount
+    doc.party_balance_after_transaction = doc.party_balance_before_transaction + doc.outstanding_amount
